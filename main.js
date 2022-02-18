df16a73e6c11d2c06c9713aaa5e5ebe2b91dc169
cchavezmx <cchavezmx@outlook.com>
2022-02-17 21:26:39 -0600

    se añade fin de ejercicio

--- main.js
+++ main.js
@@ -6,20 +6,6 @@ const input = document.querySelector('#search')
 // filtrado sera el ancla donde yo "renderize" o pintar en pantalla los resulados de la bussqueda
 const filtrado = document.querySelector('#filtrado') 
 
-const resultados = data.filter((perrito) => {
-  // en un regex la g es para que sea g es para que sea de forma global
-  // en un regex la i es que no a ser case sensitive
-  const regex = new RegExp(input, 'gi') 
-  /*
-    Metodos de regex => match, matchAll, replace
-    match = encontrar una similitud
-    Thai %& Ridgeback , >
-    replace =   ('Tahi', 'german')
-    Thai Ridgeback
-  */
-  return perrito.name.match(regex)
-})
-
 // addEventListener
 /*
   Metodo que escucha eventos dentro del DOM
@@ -31,7 +17,55 @@ const resultados = data.filter((perrito) => {
 // caputurar el evento keypress del input
 input.addEventListener('keypress', (e) => {
   // e es un objeto que contiene toda la informacion de la etiqueta 
-  console.log(e.target.value)
-})
+  let keyword = e.target.value
+  console.log(keyword)
+  const resultados = data.filter((perrito) => {
+    // en un regex la g es para que sea g es para que sea de forma global
+    // en un regex la i es que no a ser case sensitive
+    const regex = new RegExp(keyword, 'gi') 
+    /*
+      Metodos de regex => match, matchAll, replace
+      match = encontrar una similitud
+      Thai %& Ridgeback , >
+      replace =   ('Tahi', 'german')
+      Thai Ridgeback
+    */
+    return perrito.name.match(regex)
+  })
 
-console.log(resultados)
+  // filger retorna un array
+  // resultados.map(perrito => {
+  //   console.log(perrito.name)
+  // })
+//     for(const perrito of resultados) {
+    
+//     let div = document.createElement("div");
+//     let h4 = document.createElement("h4");
+//     let img = document.createElement("img");
+//     let p = document.createElement("p");
+
+//     filtrado.append(div);
+//     div.append(img);
+//     div.append(h4);
+//     div.append(p);
+
+//     // add class to div container
+//     div.classList.add('card_perrito');  
+
+//     img.src = perrito.image['url'];
+//     h4.innerText = perrito.name;
+//     p.innerText = perrito.temperament;
+// }   
+  filtrado.innerHTML = resultados.map(perrito => {
+    return (
+      `
+      <div class="card_perrito">
+        <img src="${perrito.image.url}">
+        <h4>${perrito.name}</h4>
+        <p>${perrito.temperament}</p>
+      </div>
+      `
+    )
+  })
+
+})
